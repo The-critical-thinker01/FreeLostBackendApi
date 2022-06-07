2b34616f3844528c146fe94e9b46229b49ab42f6
fred-dev22 <fredypone22@gmail.com>
2022-06-07 13:58:00 +0100

    create authentification route

--- database/models/user.model.js
+++ database/models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 const userSchema = schema({
     username: { type: String, required: true, unique: true },
@@ -10,6 +11,13 @@ const userSchema = schema({
 
 
 });
+
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hash(password, 12);
+}
+userSchema.methods.comparePassword = function(password) {
+    return bcrypt.compare(password, this.password)
+}
 
 
 const User = mongoose.model("user", userSchema);
--- queries/user.queries.js
+++ queries/user.queries.js
@@ -1,5 +1,21 @@
 const User = require('../database/models/user.model');
 
-exports.listUsers = () => {
-    return User.find().exec();
+
+exports.createUser = async(user) => {
+    try {
+        const hashedPassword = await User.hashPassword(user.password);
+        const newUser = new User({
+            username: user.username,
+            email: user.email,
+            password: hashedPassword,
+            numero: user.numero
+        });
+        return newUser.save();
+    } catch (e) {
+        throw e;
+    }
+}
+
+exports.findUserPerEmail = async(email) => {
+    return User.findOne({ 'email': email }).exec();
 }
