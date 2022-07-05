fe9e3a6e4ede8788fec93500c11e1b87aa42435b
fred-dev22 <fredypone22@gmail.com>
2022-07-05 17:41:46 +0100

    local strategy factory

--- app.js
+++ app.js
@@ -8,16 +8,15 @@ const cors = require('cors'); // pour permettre a mon backend d'accpeter les req
 exports.app = app;
 require('./config/session.config');
 require('./config/passport.config');
-const bodyParser=require('body-parser');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('short'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(index);
 
 
