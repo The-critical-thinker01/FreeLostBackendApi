9148481e8c8c45eba61652bc8b1b7aa78c0f120e
fred-dev22 <fredypone22@gmail.com>
2022-07-08 03:06:12 +0100

    add avatar field on user schema

--- database/models/user.model.js
+++ database/models/user.model.js
@@ -3,28 +3,25 @@ const schema = mongoose.Schema;
 const bcrypt = require("bcrypt");
 
 const userSchema = schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String},
-    numero: { type: Number},
-    avatar: { type: String },
-    googleId:{type:String}
-
-
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String },
+  numero: { type: Number },
+  avatar: { type: String, default: "/images/profil.jpeg" },
+  googleId: { type: String },
 });
 
-userSchema.statics.hashPassword = function(password) {
-    return bcrypt.hash(password, 12);
-}
-userSchema.methods.comparePassword = async function(password) {
-    let rps;
- await bcrypt.compare(password, this.password).then(function(result){
-        console.log(result)
-        rps=result;
-    })
-    return rps;
-}
-
+userSchema.statics.hashPassword = function (password) {
+  return bcrypt.hash(password, 12);
+};
+userSchema.methods.comparePassword = async function (password) {
+  let rps;
+  await bcrypt.compare(password, this.password).then(function (result) {
+    console.log(result);
+    rps = result;
+  });
+  return rps;
+};
 
 const User = mongoose.model("user", userSchema);
 
