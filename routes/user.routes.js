58e256a7f3ef4f4b6a75c898fe0f3a8c8a48d331
fred-dev22 <fredypone22@gmail.com>
2022-07-09 21:51:58 +0100

    ulter configuration

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,11 @@
-const router = require('express').Router();
-const { userCreate } = require('../controllers/user.controller');
+const router = require("express").Router();
+const {
+  userCreate,
+  userUpdate,
+  uploadImage,
+} = require("../controllers/user.controller");
 
-
-
-router.post('/new', userCreate);
-
-
+router.post("/new", userCreate);
+router.post("/update/avatar", uploadImage);
 
 module.exports = router;
